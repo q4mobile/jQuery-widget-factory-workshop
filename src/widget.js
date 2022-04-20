55b8877c19bb3b6225d7eac027f88744fe630367
alexrobertson-q4 <alexander.robertson@q4inc.com>
2022-04-20 10:24:50 -0400

    fetch press release data + siteCode option

--- src/widget.js
+++ src/widget.js
@@ -3,19 +3,45 @@ window.$ = window.jQuery = jquery;
 require("jquery-ui-dist/jquery-ui.min.js");
 var Mustache = require('mustache');
 
+
 $(function () {
+    async function fetchPressReleases() {
+        $.ajax({
+            type: "GET",
+            url: 'https://deltaclonesandbox.q4web.com/feed/PressRelease.svc/GetPressReleaseList?LanguageId=1&bodyType=0&pressReleaseDateFilter=3&categoryId=1cb807d2-208f-4bc3-9133-6a9ad45ac3b0&pageSize=-1&pageNumber=0&tagList=&includeTags=true&excludeSelection=1',
+            dataType: 'json'
+          }).done(function(json){
+              console.log(json);
+            $('body').html(JSON.stringify(json));
+          });
+    }
+
     $.widget("q4.newWidget", {
         options: {
             text: "I will be randomized",
+            siteCode: 'deltaclonesandbox',
             onDestroy: function () {
                 console.log('The widget has been destroyed');
             }
         },
 
         _create: function () {
+            let siteCode = this.option('siteCode')
+            this._fetchPressReleases(siteCode);
             this._refresh();
             this._on($('#clickMeButton'), {
                 click: "randomizeText"
+            });
+        },
+
+        _fetchPressReleases: function (siteCode) {
+            $.ajax({
+                type: "GET",
+                url: `https://${siteCode}.q4web.com/feed/PressRelease.svc/GetPressReleaseList?LanguageId=1&bodyType=0&pressReleaseDateFilter=3&categoryId=1cb807d2-208f-4bc3-9133-6a9ad45ac3b0&pageSize=-1&pageNumber=0&tagList=&includeTags=true&excludeSelection=1`,
+                dataType: 'json'
+            }).done(function(json){
+                console.log(json);
+                // json is the response data
             });
         },
 
@@ -48,7 +74,8 @@ $(function () {
     });
 
     $("#my-widget").newWidget({
-        text: "Hello World"
+        text: "Hello World",
+        siteCode: 'studioclassic2018na1'
     });
 
     $("#my-widget").newWidget('randomizeText');
