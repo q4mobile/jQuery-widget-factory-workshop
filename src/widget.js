0abbaa0ce7ff1e1e524b711c2c7350b22a06a3c4
q4lgc <95712594+q4lgc@users.noreply.github.com>
2022-04-20 11:28:34 -0400

    get data render template

--- src/widget.js
+++ src/widget.js
@@ -1,55 +1,63 @@
 var jquery = require("jquery");
 window.$ = window.jQuery = jquery;
 require("jquery-ui-dist/jquery-ui.min.js");
-var Mustache = require('mustache');
+var Mustache = require("mustache");
 
 $(function () {
-    $.widget("q4.newWidget", {
-        options: {
-            text: "I will be randomized",
-            onDestroy: function () {
-                console.log('The widget has been destroyed');
-            }
-        },
+  $.widget("q4.newWidget", {
+    options: {
+      toRender: "",
+      onDestroy: function () {
+        console.log("The widget has been destroyed");
+      },
+      beforeRender: function (event, data) {
+        this._trigger();
+      },
+    },
 
-        _create: function () {
-            this._refresh();
-            this._on($('#clickMeButton'), {
-                click: "randomizeText"
-            });
-        },
+    _create: function () {
+      var _ = this;
+      $.ajax({
+        type: "GET",
+        url: "https://deltaclonesandbox.q4web.com/feed/PressRelease.svc/GetPressReleaseList?LanguageId=1&bodyType=0&pressReleaseDateFilter=3&categoryId=1cb807d2-208f-4bc3-9133-6a9ad45ac3b0&pageSize=-1&pageNumber=0&tagList=&includeTags=true&excludeSelection=1",
+        dataType: "json",
+      }).done(function (json) {
+        _.option({
+          toRender: json.GetPressReleaseListResult,
+        });
+      });
+    },
 
-        randomizeText: function () {
-            this.option({
-                text: (Math.random() + 1).toString(36).substring(7)
-            });
-        },
+    _render: function () {
+      var _ = this;
+      var toRender = _.options.toRender;
+      var template = "{{#.}}" + "<h2>{{Headline}}</h2>" + "{{/.}}";
+      console.log(toRender);
 
-        _refresh: function () {
-            $('#my-widget').html('<p>' + this.options.text + '</p>')
-        },
+      var output = Mustache.render(template, toRender);
+      $("#my-widget").html(output);
+    },
 
-        _destroy: function () {
-            this._trigger('onDestroy');
-        },
-        
-        _setOption: function (key, value) {
-            if (key === "text") {
-                value = value + " appendedText"
-            }
-            this._super(key, value);
-        },
+    _refresh: function () {},
 
-        _setOptions: function () {
-            this._superApply(arguments);
-            this._refresh();
-        },
+    _destroy: function () {
+      this._trigger("onDestroy");
+    },
 
-    });
+    _setOptions: function () {
+      this._superApply(arguments);
+      this._refresh();
+    },
 
-    $("#my-widget").newWidget({
-        text: "Hello World"
-    });
+    _setOption: function (key, value) {
+      this._super(key, value);
+      if (key === "toRender") {
+        this._render();
+      }
+    },
+  });
 
-    $("#my-widget").newWidget('randomizeText');
+  $("#my-widget").newWidget({
+    key: "value",
+  });
 });
