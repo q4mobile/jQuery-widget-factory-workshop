239e8168dd44d3945bca9b0c46317740f63c40d0
Zalmy K <60475531+zkarimi22@users.noreply.github.com>
2022-04-20 11:39:21 -0400

    completed with Sergii, Tanwir and Laura

--- src/widget.js
+++ src/widget.js
@@ -1,41 +1,86 @@
 var jquery = require("jquery");
 window.$ = window.jQuery = jquery;
 require("jquery-ui-dist/jquery-ui.min.js");
-var Mustache = require('mustache');
+var Mustache = require("mustache");
 
 $(function () {
     $.widget("q4.newWidget", {
         options: {
+            limit:4,
+            limitState:false,
             text: "I will be randomized",
+            containerId: "new-container",
+            dataObj: { items: [] },
             onDestroy: function () {
-                console.log('The widget has been destroyed');
-            }
+                console.log("The widget has been destroyed");
+            },
         },
-
         _create: function () {
             this._refresh();
-            this._on($('#clickMeButton'), {
-                click: "randomizeText"
+            this.getData();
+            this._on($("#clickMeButton"), {
+                click: "changeLimitState",
             });
         },
+        getData: function () {
+            console.log("data is here");
+            var itemsObj = {};
+            $.ajax({
+                type: "GET",
+                context: this,
+                url: "https://deltaclonesandbox.q4web.com/feed/PressRelease.svc/GetPressReleaseList?LanguageId=1&bodyType=0&pressReleaseDateFilter=3&categoryId=1cb807d2-208f-4bc3-9133-6a9ad45ac3b0&pageSize=-1&pageNumber=0&tagList=&includeTags=true&excludeSelection=1",
+                dataType: "json",
+            }).done(function (json) {
+                var headlineData = json.GetPressReleaseListResult.map(function (item) {
+                    return { headline: item.Headline };
+                });
 
-        randomizeText: function () {
-            this.option({
-                text: (Math.random() + 1).toString(36).substring(7)
+                itemsObj = { items: headlineData };
+
+                this.beforeRender(itemsObj);
+
+                this.initialize(this.options.dataObj);
             });
+        },
+        beforeRender: function (itemsObj) {
+            //we do our manipulation here
+
+            limitObj = this.options.limitState ? itemsObj.items.slice(0,this.options.limit) : itemsObj.items;
+
+            itemsObj = limitObj.map(function (item) {
+                return { headline: `HEADLINE: ${item.headline}` };
+            });
+
+            this.options.dataObj.items = itemsObj;
+        },
+        initialize: function () {
+            console.log("initialize function");
+            var template = `{{#items}}
+                                    <p>{{headline}}</p>
+                                {{/items}}`;
+
+            var rendered = Mustache.render(template, this.options.dataObj);
+
+            document.getElementById(this.options.containerId).innerHTML = rendered;
+            // $('#my-widget').html(headlineData);
+        },
+        changeLimitState: function () {
+            console.log('change');
+             this.options.limitState = !this.options.limitState;
+             this._refresh();
         },
 
         _refresh: function () {
-            $('#my-widget').html('<p>' + this.options.text + '</p>')
+            this.getData();
         },
 
         _destroy: function () {
-            this._trigger('onDestroy');
+            this._trigger("onDestroy");
         },
-        
+
         _setOption: function (key, value) {
             if (key === "text") {
-                value = value + " appendedText"
+                value = value + " appendedText";
             }
             this._super(key, value);
         },
@@ -44,12 +89,10 @@ $(function () {
             this._superApply(arguments);
             this._refresh();
         },
-
     });
 
     $("#my-widget").newWidget({
-        text: "Hello World"
+        text: "Hello World",
     });
-
-    $("#my-widget").newWidget('randomizeText');
+ 
 });
