773d9a28becced878a5befb499f545ac17f292f2
Jess-Moore-Q4 <jessica.moore@q4inc.com>
2022-04-20 10:44:25 -0400

    Revert "Strip widget to bhaags blank widget"
    This reverts commit f806dedfbefd4803624f9914c728da80d47ee112.

--- src/widget.js
+++ src/widget.js
@@ -6,20 +6,50 @@ var Mustache = require('mustache');
 $(function () {
     $.widget("q4.newWidget", {
         options: {
-        ​},
-        _create: function () {
+            text: "I will be randomized",
+            onDestroy: function () {
+                console.log('The widget has been destroyed');
+            }
         },
-        _destroy: function () {},
 
+        _create: function () {
+            this._refresh();
+            this._on($('#clickMeButton'), {
+                click: "randomizeText"
+            });
+        },
+
+        randomizeText: function () {
+            this.option({
+                text: (Math.random() + 1).toString(36).substring(7)
+            });
+        },
+
+        _refresh: function () {
+            $('#my-widget').html('<p>' + this.options.text + '</p>')
+        },
+
+        _destroy: function () {
+            this._trigger('onDestroy');
+        },
+        
         _setOption: function (key, value) {
+            if (key === "text") {
+                value = value + " appendedText"
+            }
             this._super(key, value);
         },
-        ​
+
         _setOptions: function () {
             this._superApply(arguments);
+            this._refresh();
         },
-    });​
+
+    });
+
     $("#my-widget").newWidget({
-        key: "value"
-    });;
+        text: "Hello World"
+    });
+
+    $("#my-widget").newWidget('randomizeText');
 });
