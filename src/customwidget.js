5d7675edab0b0f6c1419d90580eca583b1251adf
ajibolanle-aladejubelo-q4 <ajibolanle.aladejubelo@q4inc.com>
2022-04-20 12:07:33 -0400

    step 4 complete

--- src/customwidget.js
+++ src/customwidget.js
@@ -5,49 +5,46 @@ require("jquery-ui-dist/jquery-ui.min.js");
 var Mustache = require("mustache");
 
 $(function () {
-    $.widget("q4.pressWidget", {
-        options: {
-
-        },
-        _create: function () {
-            var data = {};
-            var _ = this;
-            $.ajax({
-                type: "GET",
-                url: "https://deltaclonesandbox.q4web.com/feed/PressRelease.svc/GetPressReleaseList?LanguageId=1&bodyType=0&pressReleaseDateFilter=3&categoryId=1cb807d2-208f-4bc3-9133-6a9ad45ac3b0&pageSize=-1&pageNumber=0&tagList=&includeTags=true&excludeSelection=1",
-                dataType: "json",
-            }).done(function (json) {
-                data = json;
-                // $("body").html(JSON.stringify(data));
-                _._renderHeadline(data.GetPressReleaseListResult);
+  $.widget("q4.pressWidget", {
+    options: {
+        beforeRender: function (e, data) {
+            $(data.GetPressReleaseListResult).each(function (index, item) {
+                item.Headline = "HEADLINE: " + item.Headline;
             });
+          },
+    },
+    _create: function () {
+      var data = {};
+      var o = this.options;
+      var _ = this;
+      $.ajax({
+        type: "GET",
+        url: "https://deltaclonesandbox.q4web.com/feed/PressRelease.svc/GetPressReleaseList?LanguageId=1&bodyType=0&pressReleaseDateFilter=3&categoryId=1cb807d2-208f-4bc3-9133-6a9ad45ac3b0&pageSize=-1&pageNumber=0&tagList=&includeTags=true&excludeSelection=1",
+        dataType: "json",
+      }).done(function (json) {
+        data = json;
+        _._trigger('beforeRender', null, data);
 
+        _._renderHeadline(data.GetPressReleaseListResult);
+      });
+    },
+    _renderHeadline: function (prListResult) {
+      var ourBanana = '<div class="headline">' + "{{#.}}" + "<h2>{{Headline}}</h2>" + "{{/.}}" + "</div>";
 
-        },
-        _renderHeadline: function (prListResult) {
-            var ourBanana = (
-                '<div class="headline">' +
-                '{{#.}}' +
-                '<h2>{{Headline}}</h2>' +
-                '{{/.}}' +
-                '</div>'
-            );
+      data = Mustache.render(ourBanana, prListResult);
+      $("body").html(data);
+    },
 
-            data = Mustache.render(ourBanana, prListResult);
-            //console.log(data);
-            $("body").html(data);
-        },
+    _destroy: function () {},
 
-        _destroy: function () {},
-
-        _setOption: function (key, value) {
-            this._super(key, value);
-        },
-        _setOptions: function () {
-            this._superApply(arguments);
-        },
-    });
-    $("#my-widget").pressWidget({
-        key: "value",
-    });
+    _setOption: function (key, value) {
+      this._super(key, value);
+    },
+    _setOptions: function () {
+      this._superApply(arguments);
+    },
+  });
+  $("#my-widget").pressWidget({
+    key: "value",
+  });
 });
