e642080cf6aab4a1a03c3a423cf283a997addeba
Abner Rojas <abner.rojas@144.studio>
2022-04-20 11:25:40 -0400

    we done

--- src/widget.js
+++ src/widget.js
@@ -6,50 +6,97 @@ var Mustache = require('mustache');
 $(function () {
     $.widget("q4.newWidget", {
         options: {
-            text: "I will be randomized",
-            onDestroy: function () {
-                console.log('The widget has been destroyed');
-            }
+            toRender: "",
+            limit: 0,
+            showLimitItems: false,
+            beforeRender: function() {},
         },
 
         _create: function () {
-            this._refresh();
-            this._on($('#clickMeButton'), {
-                click: "randomizeText"
+            var _ = this;
+            _._getData();
+
+            this._on($('#clickMeButton'),{
+                click: '_toggleFilterData'
             });
         },
 
-        randomizeText: function () {
-            this.option({
-                text: (Math.random() + 1).toString(36).substring(7)
+        _getData: function (limit=-1){
+
+            var _ = this;
+            $('#clickMeButton').attr("disabled", true).text('Loading...');
+
+            $.ajax({
+                type: "GET",
+                url: 'https://deltaclonesandbox.q4web.com/feed/PressRelease.svc/GetPressReleaseList?LanguageId=1&bodyType=0&pressReleaseDateFilter=3&categoryId=1cb807d2-208f-4bc3-9133-6a9ad45ac3b0&pageSize=' + limit + '&pageNumber=0&tagList=&includeTags=true&excludeSelection=1',
+                dataType: 'json'
+            }).done(function (json) {
+                _.option({
+                    toRender: json.GetPressReleaseListResult
+                });
+                
+                $('#clickMeButton').attr("disabled", false).text('Toggle Limit');
             });
         },
 
-        _refresh: function () {
-            $('#my-widget').html('<p>' + this.options.text + '</p>')
+        _toggleFilterData: function () {
+            var _ = this,
+                limit = _.options.limit,
+                showLimitItems = !_.options.showLimitItems
+            
+            var newLimit = limit !== 0 && showLimitItems ? limit : -1
+
+            _.option({
+                showLimitItems: showLimitItems
+            });
+
+            console.log(_.options.showLimitItems);
+
+            _._getData(newLimit);
         },
 
-        _destroy: function () {
-            this._trigger('onDestroy');
+
+        _render: function() {
+            var _ = this;
+            data = _.options.toRender;
+
+            // if (_.options.limit > 0){
+            //     data = data.slice(0, _.options.limit);
+            // }
+
+            console.log(data);
+            _._trigger('beforeRender', null, [data]);
+
+            var template = (
+                '{{#.}} ' +
+                    '<p>{{Headline}}</p> ' +
+                '{{/.}} '
+            )
+            var output = Mustache.render(template, data);
+            $('#my-widget').html(output);
         },
-        
+
+        _destroy: function () {},
+
         _setOption: function (key, value) {
-            if (key === "text") {
-                value = value + " appendedText"
-            }
             this._super(key, value);
+            if (key === "toRender") {
+                this._render();
+            }
         },
 
         _setOptions: function () {
             this._superApply(arguments);
-            this._refresh();
         },
 
     });
 
     $("#my-widget").newWidget({
-        text: "Hello World"
+        limit: 4,
+        beforeRender: function (e, data) {
+            $.each(data, function(i, item) {
+                item.Headline = i+" HEADLINE: " + item.Headline;
+            }) 
+        }
     });
-
-    $("#my-widget").newWidget('randomizeText');
 });
